cb90b99e5bb34b1320618933c3f4d1b2579e020d
Krishna <scuffedsweat006@gmail.com>
2025-04-26 13:11:36 +0530

    SearchBar commit

--- sem2capstone/app/page.js
+++ sem2capstone/app/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from '@/components/NavBar'
+import SearchBar from '@/components/SearchBar'
 function page() {
   return (
     <div className = "container">
     <NavBar/>
+    <SearchBar/>
     </div>
   )
 }
