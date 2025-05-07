386f1a994f6c8b462ce64365faf1a1868d6c6268
Krishna <scuffedsweat006@gmail.com>
2025-05-07 17:16:22 +0530

    next.json config change

--- sem2capstone/next.config.js
+++ sem2capstone/next.config.js
@@ -1,8 +1,8 @@
-// next.config.js
 const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
 
 module.exports = withFlowbiteReact({
   images: {
-    domains: ['encrypted-tbn0.gstatic.com'], // This allows loading external images from this domain
+    domains: ['encrypted-tbn0.gstatic.com'],
   },
+  output: 'export',
 });
