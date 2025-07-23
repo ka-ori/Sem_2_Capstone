9594c2b38cc364e7da7de3a67ed704bf9bd815da
Krishna <scuffedsweat006@gmail.com>
2025-07-23 23:49:39 +0530

    Label Input Fix

--- sem2capstone/app/login/page.js
+++ sem2capstone/app/login/page.js
@@ -128,6 +128,9 @@ const BottomGradient = () => {
     </>
   )
 }
+const LabelInputContainer = ({ children, className }) => {
+  return <div className={cn("!flex !w-full !flex-col !space-y-2", className)}>{children}</div>
+}
 
 
 
